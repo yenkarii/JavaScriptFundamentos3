3d25d3eb300eab8b0185881bdbb02b40e731a655
yenkarii <guilherme.antonio1702@gmail.com>
2026-08-20 11:38:28 -0300

    Update

--- 03-objetos/01-criando-objetos.js
+++ 03-objetos/01-criando-objetos.js
@@ -1,1 +1,6 @@
-
+const aluno = {
+    nome: "Rafael",
+    idade: 16,
+    nota: 7
+};
+console.log(aluno);
--- 03-objetos/02-acessando-propriedades.js
+++ 03-objetos/02-acessando-propriedades.js
@@ -1,1 +1,7 @@
-
+const aluno = {
+    nome: "Sofia",
+    idade: 17,
+    nota: 8,
+};
+console.log(aluno.nome);
+console.log(aluno.idade);
