72f44815f29b1273e6015da99357b91184f81497
yenkarii <guilherme.antonio1702@gmail.com>
2026-08-20 16:34:09 -0300

    aaaaa

--- 06-introducao-poo/05-classe-produtos.js
+++ 06-introducao-poo/05-classe-produtos.js
@@ -7,8 +7,13 @@ class Produto{
     aplicarDesconto(percentual){
         this.preco -= (this.preco * (percentual / 100));
     }
+
+    valorAPagar(){
+        console.log(`Valor a pagar = R$${this.preco.toFixed(2)}`)
+    }
 }
 
 const produto = new Produto("Mouse", 100);
 produto.aplicarDesconto(10);
 console.log(produto);
+produto.valorAPagar();
