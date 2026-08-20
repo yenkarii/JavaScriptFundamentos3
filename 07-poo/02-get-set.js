75cd2c49577b23e4e04e5ec2b48187530b6b6828
yenkarii <guilherme.antonio1702@gmail.com>
2026-08-20 16:41:26 -0300

    Aaa

--- 07-poo/02-get-set.js
+++ 07-poo/02-get-set.js
@@ -2,7 +2,7 @@ class Pessoa {
     #nome;
 
     constructor(nome) {
-        this.nome = nome;
+        this.#nome = nome;
     }
 
     get nome() {
@@ -13,3 +13,6 @@ class Pessoa {
         if (valor.lenght >= 2) this.#nome = valor;
     }
 }
+
+const pessoa = new Pessoa("Pedro");
+console.log(pessoa.nome);
